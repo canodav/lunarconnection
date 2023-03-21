70b50f1d0d4867741d55aa7c932f648ecd40c07c
canodav <david.cano02@estudiant.upf.edu>
2023-03-21 14:44:39 +0100

    api url

--- src/routes/results/+page.js
+++ src/routes/results/+page.js
@@ -6,10 +6,10 @@ export async function load ({url}) {
     const date1 = url.searchParams.get('date1') 
     const date2 = url.searchParams.get('date2') 
 
-    let moon1 = await fetch(`http://localhost:5173/api/moon?date=${date1}`);
+    let moon1 = await fetch(`https://lunarconnection.org/api/moon?date=${date1}`);
     moon1 = await moon1.json();
 
-    let moon2 = await fetch(`http://localhost:5173/api/moon?date=${date2}`);
+    let moon2 = await fetch(`https://lunarconnection.org/api/moon?date=${date2}`);
     moon2 = await moon2.json();
 
     return {
